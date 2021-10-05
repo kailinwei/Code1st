c12b9d14bbb08ea0964f5cfb0f9e284c8a0641fd
kailinwei <kailinwei95@gmail.com>
2021-10-05 11:19:33 -0700

    first commit

---
+++ Models/Team.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Code1st.Models
+{
+    public class Team
+    {
+        [Key]
+        public string TeamName { get; set; }
+        public string City { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
